84cd367a28d69dc3dbb03fa14b839db83bd4ef4b
Ivan Carrasco <ivancarrascocano@gmail.com>
2026-02-11 20:10:25 -0500

    Beta 1.0

---
+++ carrusel.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const slides = document.querySelectorAll('.slider-container img');
+    let currentSlide = 0;
+    
+    function nextSlide() {
+        slides[currentSlide].classList.remove('active');
+        
+        currentSlide = (currentSlide + 1) % slides.length;
+        
+        slides[currentSlide].classList.add('active');
+    }
+    
+    setInterval(nextSlide, 5000);
+});
